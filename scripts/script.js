2327c9a498464b8018fbdccff0440ffd3a3e4503
williamyap90 <william.yap90@gmail.com>
2021-06-14 14:06:15 +0100

    Reordered code, changed computerPlay as it was possible to return zero.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,15 @@
-//create function that randomly returns rock paper or scissors (CPU)
-//create function that plays single round 
-//create a game function that plays 5 rounds
+// Player & CPU selection
+const playerSelection = 'Scissors'
+const computerSelection = computerPlay();
 
-// Function to generate random choice
+
+// Function to generate random choice for CPU
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
 
-    return options[Math.floor(Math.random() * options.length)];
+    return options[Math.floor(Math.random() * 3 + 1)];
 }
 
-
-const playerSelection = 'roCk'
-const computerSelection = computerPlay();
 
 // Function to play one round
 function playRound(playerSelection, computerSelection) {
@@ -19,35 +17,37 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === 'rock') {
         if (computerSelection === "scissors") {
-            return 'You Win! Rock beats Scissors.'
+            return 'You Win! Rock beats Scissors.';
         } else if (computerSelection === "paper") {
-            return 'You Lose! Paper beats Rock.'
+            return 'You Lose! Paper beats Rock.';
         }
     }
-
     if (playerSelection === 'paper') {
         if (computerSelection === "rock") {
-            return 'You Win! Paper beats Rock.'
+            return 'You Win! Paper beats Rock.';
         } else if (computerSelection === "scissors") {
-            return 'You Lose! Scissors beats Paper.'
+            return 'You Lose! Scissors beats Paper.';
         }
     }
-
     if (playerSelection === 'scissors') {
         if (computerSelection === "paper") {
-            return 'You Win! Scissors beats Paper.'
+            return 'You Win! Scissors beats Paper.';
         } else if (computerSelection === "rock") {
-            return 'You Lose! Rock beats Scisssors.'
+            return 'You Lose! Rock beats Scisssors.';
         }
     }
-
     if (playerSelection === computerSelection) {
         return "It's a draw!";
     }
+}
+
+
+// Function for game to play 5 rounds
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
 
 }
 
-console.log(playerSelection);
-console.log(computerPlay());
-console.log(playRound(playerSelection, computerSelection));
 
+console.log(playRound(playerSelection, computerSelection));
